13b4ef66ddce31fe1f8c59c47261f30a4f6dd12f
unknown <j.mager@student.alfa-college.nl>
2016-12-12 09:36:24 +0100

    sectie 7 initial commit

---
+++ sectie7-1.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <p>Prijs in bitcoin is: <script>document.write(bitcoin)</script></p>
+
+        <input type="text" id="email" placeholder="email"><br>
+
+        <?php
+        function generateRandomNumber($length) {
+            $characters = '0123456789';
+            $charactersLength = strlen($characters);
+            $randomString = '';
+            for ($i = 0; $i < $length; $i++) {
+                $randomString .= $characters[rand(0, $charactersLength - 1)];
+            }
+            return $randomString;
+        }
+        echo generateRandomNumber(10);
+        ?><br>
+        <input type="submit" value="betalen">
+
+    </form>
+</body>
+</html>

1d36ed8cd5f04dfc77100ab6bba3cfe636ed9bb3
unknown <j.mager@student.alfa-college.nl>
2016-12-12 10:14:34 +0100

    stylesheet toegevoegd

--- sectie7-2.php
+++ sectie7-2.php
@@ -13,7 +13,7 @@ gemaakt op: 12-12-2016
 <!DOCTYPE html>
 <html>
 <head>
-
+    <link rel="stylesheet" href="style.css">
     <style type="text/css">
         .tg  {border-collapse:collapse;border-spacing:0;}
         .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
